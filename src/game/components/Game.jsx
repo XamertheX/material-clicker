14c257fb5a1a802b8f7a18bf9d8b43c34d31ae91
Dave <imdaveead@gmail.com>
2019-02-07 10:40:08 -0500

    Add React and Game Component

---
+++ src/game/components/Game.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    state = {  }
+    render() { 
+        return <>
+            <h1>Material Clicker</h1>
+        </>;
+    }
+}
+ 
+export default Game;
--- src/game/index.jsx
+++ src/game/index.jsx
@@ -5,3 +5,10 @@ console.log("👋 Hello Material Clcker!");
 
 // Import Global Styles
 import './global.css';
+
+// Render <Game />
+import React from 'react';
+import { render } from 'react-dom';
+import Game from './components/Game';
+
+render(<Game />, document.getElementById('root'));

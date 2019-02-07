6323ff692eae8a4ef4a5fc41927639e4a15a8808
Dave <imdaveead@gmail.com>
2019-02-07 12:05:09 -0500

    Remove Electron Security Warnings

--- src/main/preload.js
+++ src/main/preload.js
@@ -2,3 +2,10 @@
 // Script loaded in the renderer process before all other code, and has access
 // to all Node APIs.
 //
+
+// Remove Electron Security Warnings
+const { warn } = console;
+console.warn = (...args) => {
+    /^%cElectron Security Warning/.test(args[0])
+        || Reflect.apply(warn, console, args);
+};

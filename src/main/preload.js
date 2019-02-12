bd8588866d8c1e970bd70af9aa037e3fce0f12f5
Dave <imdaveead@gmail.com>
2019-02-12 15:01:18 -0500

    On error, open dev tools

--- src/main/preload.js
+++ src/main/preload.js
@@ -3,12 +3,28 @@
 // to all Node APIs.
 //
 
+function openDevTools() {
+  require('electron').remote.getCurrentWebContents().openDevTools();
+  require('electron').remote.getCurrentWebContents().devToolsWebContents.focus();
+}
+
 // Remove Electron Security Warnings
 /* eslint-disable no-console */
 const warn = console.warn;
 console.warn = (...args) => {
   if (!/^%cElectron Security Warning/.test(args[0])) {
     Reflect.apply(warn, console, args);
+    openDevTools();
   }
+};
+/* eslint-enable no-console */
+
+// If error is thrown open dev tools to show this
+// FIXME: Disable this on production.
+/* eslint-disable no-console */
+const error = console.error;
+console.error = (...args) => {
+  Reflect.apply(error, console, args);
+  openDevTools();
 };
 /* eslint-enable no-console */

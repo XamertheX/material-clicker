e4742d6852acbd68efd80b7da9951c7cc8ab1a4c
Dave <imdaveead@gmail.com>
2019-02-12 15:45:00 -0500

    Fix fullscreen

--- src/game/titlebar.jsx
+++ src/game/titlebar.jsx
@@ -2,6 +2,12 @@ import theme from './content/theme';
 import { Titlebar, Color } from 'custom-electron-titlebar';
 import MenuBar from './content/menubar';
 
+// Fix the fullscreen handler.
+Titlebar.prototype.onDidChangeFullscreen = function(full) {
+  this.titlebar.style.display = full ? 'none' : 'flex';
+  this.container.style.top = full ? '0' : '30px';
+};
+
 const titlebar = new Titlebar({
   backgroundColor: Color.fromHex(theme.palette.primary[900]),
   icon: null,

aa8cd9b817a2402233e0bc918702a4b2d354113d
Dave <imdaveead@gmail.com>
2019-02-12 17:25:45 -0500

    Better Globals ($js)

--- src/game/content/hooks/globals.jsx
+++ src/game/content/hooks/globals.jsx
@@ -15,4 +15,14 @@ Object.keys(vars).forEach((key) => {
   });
 });
 
-window.$require = require.context('../../', true, /.jsx?$/);
+const context = require.context('../../', true, /.jsx$/);
+window.$js = context
+  .keys()
+  .filter(key => !key.includes('content/'))
+  .filter(key => !key.includes('components/'))
+  .filter(key => !key.includes('pages/'))
+  .filter(key => key !== './index.jsx')
+  .filter(key => key !== './loading.jsx')
+  .map(key => [key, key.substring(2, key.length - 4)])
+  .map(([key, id]) => [key, id.replace(/\//g, '_')])
+  .reduce((prev, [key, id]) => ({ ...prev, [id]: context(key)}), {});

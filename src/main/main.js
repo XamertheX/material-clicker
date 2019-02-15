76a201fd3b95416daa1bcaac607022c789c22eb2
Dave <imdaveead@gmail.com>
2019-02-14 19:29:41 -0500

    remove squirrel updater

--- src/main/main.js
+++ src/main/main.js
@@ -9,11 +9,6 @@ const { app, BrowserWindow } = require('electron');
 // anywhere else.
 app.commandLine.appendSwitch('force-color-profile', 'srgb');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;

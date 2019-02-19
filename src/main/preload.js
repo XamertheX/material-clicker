a76036329be78921828ab8eb51bb7d818762adf4
Dave <imdaveead@gmail.com>
2019-02-18 20:39:48 -0500

    Move devtools on error back to preload

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,9 +1,32 @@
+import { remote } from "electron";
+
+// Devtools on Error
+function openDevTools() {
+  if (process.env.NODE_ENV !== 'production') {
+    const web = remote.getCurrentWebContents();
+    web.openDevTools();
+    if (web.devToolsWebContents) {
+      web.devToolsWebContents.focus();
+    }
+  }
+}
+
+// If error is thrown open dev tools to show this
+/* eslint-disable no-console */
+const error = console.error;
+console.error = (...args) => {
+  Reflect.apply(error, console, args);
+  openDevTools();
+};
+/* eslint-enable no-console */
+
 // Remove Electron Security Warnings
 /* eslint-disable no-console */
 const warn = console.warn;
 console.warn = (...args) => {
   if (!/^%cElectron Security Warning/.test(args[0])) {
     Reflect.apply(warn, console, args);
+    openDevTools();
   }
 };
 /* eslint-enable no-console */

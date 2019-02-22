1de3c6561a203132ed1ef66f830e0a127470d444
Dave <imdaveead@gmail.com>
2019-02-22 00:04:39 -0500

    Sound Fallbacks (Fix #40)

--- src/game/systems/audio.jsx
+++ src/game/systems/audio.jsx
@@ -15,11 +15,19 @@ function requireAll(r) {
 }
 requireAll(require.context('../../res/audio', true, /\.(wav|mp3|webm)?$/));
 
-export function playSound(id) {
-  if(id in registry) {
-    registry[id].play();
-  } else {
-    // eslint-disable-next-line
-    console.error(`Audio Error: Cannot find sound ${id}!`);
+export function playSound(...ids) {
+  for (let i = 0; i < ids.length; i++) {
+    if (ids[i] in registry) {
+      registry[ids[i]].play();
+      return;
+    }
+  }
+  if (ids.length === 1) {
+    // eslint-disable-next-line no-console
+    console.error(`Audio Error: Cannot find sound ${ids[0]}!`);
+  }
+  if (ids.length > 1) {
+    // eslint-disable-next-line no-console
+    console.error(`Audio Error: Cannot find fallback sound ${ids[ids.length - 1]}!`);
   }
 }

4cc57de7a2da9a6a38e1c46f4861f8ececdef946
Dave <dave@davecode.me>
2019-03-04 12:04:52 -0500

    Probably Automatically fix the day rollover problem (fix #51)

--- src/game/content/hooks/daily.jsx
+++ src/game/content/hooks/daily.jsx
@@ -9,75 +9,99 @@ let dayMap = ['Sunday',
   'Friday',
   'Saturday',
 ];
+
 let dayNumber = new Date().getDay();
 export let day = dayMap[dayNumber];
 
-if(dayNumber === 1) {
-  // Double Material
-  setVar('materialPerClickBoost', 2);
-
-  AlertDialog(
-    'Welcome!',
-    `Today is ${day}, and that means you get Double Material Per Click. All your clicks
-      will be worth double. This does not effect autoclickers.
-    `,
-    [
-      { text: 'Okay' },
-    ],
-  );
-}else if(dayNumber === 2) {
-  // Increased Golden Click
-
-  setVar('buttonDoublePercentBoost', 10);
-
-  AlertDialog(
-    'Welcome!',
-    `Today is ${day}, and that means you have a higher change of getting Golden Clicks.`,
-    [
-      { text: 'Okay' },
-    ],
-  );
-}else if(dayNumber === 3) {
-  // Wednesday Case
-  // AlertDialog(
-  //   'Welcome!',
-  //   `Today is ${day}, and that means you just got a Wednesday case! These contain
-  //     different items every week! Come back next Wednesday for a new Wednesday
-  //     case!
-  //   `,
-  //   [
-  //     { text: 'Okay' },
-  //   ],
-  // );
-}else if(dayNumber === 4) {
-  // Longer Lasting Powerups
-  // AlertDialog(
-  //   'Welcome!',
-  //   `Today is ${day}, and that means all your activated powerups last twice as long.`,
-  //   [
-  //     { text: 'Okay' },
-  //   ],
-  // );
-}else if(dayNumber === 5) {
-  // Reduced Upgrade Price
-  setVar('shopDiscount', 0.85);
-
-  AlertDialog(
-    'Welcome!',
-    `Today is ${day}, and that means all the upgrades in shop are 15% off!`,
-    [
-      { text: 'Okay' },
-    ],
-  );
-}else if(dayNumber === 6) {
-  // More Golden Material
-  // AlertDialog(
-  //   'Welcome!',
-  //   `Today is ${day}, and that means Golden Material when prestiging is increased by
-  //     50%!
-  //   `,
-  //   [
-  //     { text: 'Okay' },
-  //   ],
-  // );
+function timeUntilMidnight() {
+  var midnight = new Date();
+  midnight.setHours(24);
+  midnight.setMinutes(0);
+  midnight.setSeconds(0);
+  midnight.setMilliseconds(0);
+  return midnight.getTime() - new Date().getTime();
 }
+
+function setDaily() {
+  setVar('materialPerClickBoost', 1);
+  setVar('buttonDoublePercentBoost', 0);
+  setVar('shopDiscount', 1);
+
+  if(dayNumber === 1) {
+    // Double Material
+    setVar('materialPerClickBoost', 2);
+
+    AlertDialog(
+      'Welcome!',
+      `Today is ${day}, and that means you get Double Material Per Click. All your clicks
+        will be worth double. This does not effect autoclickers.
+      `,
+      [
+        { text: 'Okay' },
+      ],
+    );
+  }else if(dayNumber === 2) {
+    // Increased Golden Click
+
+    setVar('buttonDoublePercentBoost', 10);
+
+    AlertDialog(
+      'Welcome!',
+      `Today is ${day}, and that means you have a higher change of getting Golden
+      Clicks.`,
+      [
+        { text: 'Okay' },
+      ],
+    );
+  }else if(dayNumber === 3) {
+    // Wednesday Case
+    // AlertDialog(
+    //   'Welcome!',
+    //   `Today is ${day}, and that means you just got a Wednesday case! These contain
+    //     different items every week! Come back next Wednesday for a new Wednesday
+    //     Case!
+    //   `,
+    //   [
+    //     { text: 'Okay' },
+    //   ],
+    // );
+  }else if(dayNumber === 4) {
+    // Longer Lasting Powerups
+    // AlertDialog(
+    //   'Welcome!',
+    //   `Today is ${day}, and that means all your activated powerups last
+    //   twice as long.`,
+    //   [
+    //     { text: 'Okay' },
+    //   ],
+    // );
+  }else if(dayNumber === 5) {
+    // Reduced Upgrade Price
+    setVar('shopDiscount', 0.85);
+
+    AlertDialog(
+      'Welcome!',
+      `Today is ${day}, and that means all the upgrades in shop are 15% off!`,
+      [
+        { text: 'Okay' },
+      ],
+    );
+  }else if(dayNumber === 6) {
+    // More Golden Material
+    // AlertDialog(
+    //   'Welcome!',
+    //   `Today is ${day}, and that means Golden Material when prestiging is increased by
+    //     50%!
+    //   `,
+    //   [
+    //     { text: 'Okay' },
+    //   ],
+    // );
+  }
+
+  setTimeout(() => {
+    setDaily();
+  }, timeUntilMidnight());
+}
+
+setDaily();
